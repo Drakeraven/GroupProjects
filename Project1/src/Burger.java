621fe63dd205f8e9b2647c48733160e19cd93917
cbrwo <cbrwo@Soleil>
2017-01-20 09:30:31 -0800

    Burger prepped and ready to build upon.
    Constructor added, string holding baron burger added, skeleton structure
    of all methods added

--- Project1/src/Burger.java
+++ Project1/src/Burger.java
@@ -1,4 +1,57 @@
 
-public class Burger {
 
+public class Burger {
+	
+	private MyStack<String> myBurger; 
+	
+	private String[] baronBurger = {"Pickle", "Top Bun", 
+			"Mayonnaise", "Baron Sauce", "Lettuce", "Tomato", "Onions",
+			"Pepperjack", "Mozzarella", "Cheddar", "Patty", "Mushrooms", 
+			"Mustard", "Ketchup", "Bottom Bun"};
+
+	
+	public Burger(boolean theWorks) { 
+		if(theWorks){
+			for(int i = baronBurger.length; i >= 0; i--){
+				myBurger.push(baronBurger[i]);
+			}
+		} else { 
+			myBurger.push("Bottom Bun");
+			myBurger.push("Patty");
+			myBurger.push("Top Bun");
+		}
+			
+	}
+	
+	public void changePatties(String pattyType){
+	
+	}
+	
+	public void addPatty(){
+		
+	}
+	
+	public void removePatty(){ 
+		
+	}
+	
+	public void addCategory(String type){
+		
+	}
+	
+	public void removeCategory(String type){
+		
+	}
+	
+	public void addIngredient(String type){
+		
+	}
+	
+	public void removeIngredient(String Type){
+		
+	}
+	
+	public String toString(){
+		return ""; 
+	}
 }

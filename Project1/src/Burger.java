0c1febdaa4b88abe2a847c8a14c397b64846edf6
cbrwo <cbrwo@Soleil>
2017-01-20 17:06:17 -0800

    Burgers are for noobs anyway - Add Ing. Problem
    Still giving problems with add Ingredient, problems include out of
    bounds exceptions, incorrect looping, and infinite looping.

--- Project1/src/Burger.java
+++ Project1/src/Burger.java
@@ -4,6 +4,8 @@ public class Burger {
 	
 	private MyStack<String> myAux; 
 	
+	private String myPatty = "Beef"; 
+	
 	private String[] baronBurger = {"Pickle", "Top Bun", 
 			"Mayonnaise", "Baron Sauce", "Lettuce", "Tomato", "Onions",
 			"Pepperjack", "Mozzarella", "Cheddar", "Beef", "Mushrooms", 
@@ -49,6 +51,7 @@ public class Burger {
 	
 	public void changePatties(String pattyType){
 		//iterate through stack, find patty, pop, push new patty
+		myPatty = pattyType;
 		while (!myBurger.isEmpty()) {
 			if(myBurger.peek().equalsIgnoreCase("Beef")) {
 				myBurger.pop();
@@ -103,10 +106,14 @@ public class Burger {
 			case "sauces": category = mySauce;
 		}
 		
+		System.out.println(category.toString());
+		
 		for (int i = 0; i < category.length; i++){
+			System.out.println("removeCat item sent: " + category[i]);
 			removeIngredient(category[i]);
 		}
 	}
+	
 	
 	public void addIngredient(String type) {
 		//find the item after the ingredient in the array
@@ -119,24 +126,65 @@ public class Burger {
 				location = i + 1;
 			}
 		}
-		int checkSpot = location + 1;
-		System.out.println(baronBurger[location - 1] + "Item Above/to be placed");
-		System.out.println(baronBurger[location] + "Item below");
-		while (checkSpot < baronBurger.length || flag) { 
-			if (!myBurger.isEmpty()) {
-				if (myBurger.peek().equalsIgnoreCase(baronBurger[location])) {
+		int checkSpot = 0;
+		System.out.println(baronBurger[location - 1] + " Item Above/to be placed");
+		System.out.println(baronBurger[location] + " Item below");
+		while (location + checkSpot < baronBurger.length || flag) {   //CHANGED BOUNDS, STILL HITTING OUT OF BOUNDS EXCEPTION
+			while (!myBurger.isEmpty()) {
+				System.out.println(baronBurger[location + checkSpot]);   //PUTS ONION AFTER FIRST BEEF??? FOR SOME REASON
+				if (myBurger.peek().equalsIgnoreCase(baronBurger[location + checkSpot])) {
 					myBurger.push(type);
-					flag = true; 
+					System.out.println("Hello" + myBurger);
+					flag = true; 								//FLAG DOESN'T END THE LOOP, CONTINUES LOOPING AFTER PUSHING ONION
 				} else {
 					System.out.println(myBurger);
-					checkSpot++;
 					myAux.push(myBurger.pop());
 					//System.out.println(myAux);
-				}			
+				}	
+				System.out.println(location + checkSpot);
+				checkSpot++;
 			}
+			//checkSpot++;
+			//System.out.println(myBurger);
+			reset();
 		}
+		//System.out.println(myBurger);
 		System.out.println("reset");
 		reset();
+	}
+	
+	//REWRITING IN AN ATTEMPT TO UNDERESTAN WHATS GOING ON.
+	public void test (String type) {
+		//want to figure out what item is adjacent to the TYPE in order
+		//to put it in the right place
+		
+		int location = 0; 
+		
+		//First, figure out where the item is in the complete list 
+		for (int i = 0; i < baronBurger.length; i++) { 
+			if (type.equalsIgnoreCase(baronBurger[i])) {
+				location = i; 
+			}
+		}
+		
+		//iterate over the stack, checking if the item below it exists in the stack
+		//if not, reset. increment the baronBurger location, repeat above
+		int add = 1; 
+		boolean flag = false; 
+		while (location + add < baronBurger.length) {
+			while (!myBurger.isEmpty() || flag){
+				if (myBurger.peek().equalsIgnoreCase(baronBurger[location + add])){
+					myBurger.push(type);
+					flag = true; 
+				} else { 
+					myAux.push(myBurger.pop());
+				}
+				add++; 
+			}
+			//add++;
+			//flag = false;
+		}
+		 
 	}
 	
 	public void removeIngredient(String type) {

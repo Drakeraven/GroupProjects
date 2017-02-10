925ccca40e3521d6376d253f6499174667849dc6
steph <steph@DESKTOP-CK9F15N>
2017-02-10 11:52:53 -0800

    MESS.

---
+++ Project2/src/CharacterNode.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+
+public class CharacterNode implements Comparator<T>{
+	
+	private int count;
+	
+	public CharacterNode() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		//TODO WRITE.
+		return false;
+	}
+
+	@Override
+	public int compare(T o1, T o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- Project2/src/MyPriorityQueue.java
+++ Project2/src/MyPriorityQueue.java
@@ -1,10 +1,32 @@
+import java.util.ArrayList;
 
-public class MyPriorityQueue {
-
+public class MyPriorityQueue<T> {
+ private ArrayList<T> myDataStructure;
+ 
 	public MyPriorityQueue() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public T deleteMin(T item) { //TODO nodes! 
+		T itemTemp = myDataStructure.get(1);
+		myDataStructure.set(1, myDataStructure.get(myDataStructure.size() -1));
+		percolateDown(myDataStructure.get(1));
+		return itemTemp;
+	}
+	
+	private void percolateDown(T item) {
+		//TODO recursive 
+		if (myDataStructure.get(myDataStructure.indexOf(item) * 2).compareTo(item) < 0) {
+			T itemTemp = item;
+			myDataStructure.set(myDataStructure.indexOf(item) * 2, itemTemp);
+			myDataStructure.set(myDataStructure.indexOf(item), myDataStructure.get(myDataStructure.indexOf(item) * 2));
+		
+		} // left child
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

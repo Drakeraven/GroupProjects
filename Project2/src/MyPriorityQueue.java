c8a03ef555b10fd7efe14dadd00bbd7e86d1dc47
steph <steph@DESKTOP-CK9F15N>
2017-02-10 11:02:18 -0800

    Start of Priority Queue

---
+++ Project2/src/MyPriorityQueue.java
@@ -0,0 +1,13 @@
+
+public class MyPriorityQueue {
+
+	public MyPriorityQueue() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
